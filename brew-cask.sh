96958a14c54a6766546a2c045838ab9a57758421
Zach Nagengast <zacharynagengast@gmail.com>
2023-11-30 17:19:13 -0800

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,20 +1,19 @@
 brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro
-brew cask install font-fira-code
-brew cask install font-hack
-brew cask install google-chrome
-brew cask install visual-studio-code
-brew cask install itsycal
-brew cask install iterm2
-brew cask install blender
-brew cask install sequel-pro
-brew cask install zoomus
-brew cask install android-studio
-brew cask install docker
-brew cask install docker-toolbox
-brew cask install gimp
-brew cask install charles
-brew cask install postman
-brew cask install google-backup-and-sync
-
+brew install --cask font-source-code-pro
+brew install --cask font-fira-code
+brew install --cask font-hack
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask itsycal
+brew install --cask iterm2
+brew install --cask blender
+brew install --cask zoomus
+brew install --cask android-studio
+brew install --cask docker
+brew install --cask docker-toolbox
+brew install --cask gimp
+brew install --cask charles
+brew install --cask postman
+brew install --cask istat-menus
+brew install --cask tableplus
 
